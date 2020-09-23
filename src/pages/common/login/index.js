f013823751f22e7771dde8c3bc2f54b20d3f98b8
wq1997 <2957009914@qq.com>
2020-09-23 19:42:48 +0800

    提交

--- src/pages/common/login/index.js
+++ src/pages/common/login/index.js
@@ -1,61 +1,59 @@
 import React from 'react';
-import { Row, Col, Form, Input, Button, Checkbox } from 'antd';
+import { Row, Col, Form, Input, Button, Tabs } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import styles from './index.less';
 
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
+const { TabPane } = Tabs;
 
 const onFinish = values => {
   console.log('Success:', values);
 };
 
-const onFinishFailed = errorInfo => {
-  console.log('Failed:', errorInfo);
-};
-
 const Login = () => {
   return (
-    <Row className={styles.login}>
-      <Col span={12}>图片ß</Col>
-      <Col className={styles.loginRight}>
-        <Row className={styles.loginRightTop}>
-          <a>其他产品</a>
-          <a>墨道官网</a>
-        </Row>
-        <div className={styles.loginRightBottom}>
-          <div className={styles.loginRightBottomContent}>
-            <Form
-              name="basic"
-              initialValues={{ remember: true }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-            >
-              <Form.Item
-                label="账号"
-                name="username"
-                rules={[{ required: true, message: '请输入您的账号！' }]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                label="密码"
-                name="password"
-                rules={[{ required: true, message: '请输入您的密码！' }]}
-              >
-                <Input.Password />
-              </Form.Item>
-              <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">
-                  登录
-                </Button>
-                <Button className={styles.forgetPassWord}>忘记密码</Button>
-              </Form.Item>
-            </Form>
-          </div>
+    <div className={styles.login}>
+      <div className={styles.content}>
+        <div className={styles.contentLeft}>王清管理系统</div>
+        <div className={styles.contentRight}>
+          <Tabs defaultActiveKey="login" className={styles.contentRightTab}>
+            <TabPane tab="登录" key="login">
+              <Form name="normal_login" onFinish={onFinish}>
+                <div className={styles.title}>用户登录</div>
+                <Form.Item
+                  name="username"
+                  rules={[{ required: true, message: '请输入您的用户名!' }]}
+                >
+                  <Input prefix={<UserOutlined />} placeholder="用户名" />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: '请输入您的密码!' }]}
+                >
+                  <Input
+                    prefix={<LockOutlined />}
+                    type="password"
+                    placeholder="密码"
+                  />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className={styles.submit}
+                  >
+                    登录
+                  </Button>
+                </Form.Item>
+              </Form>
+            </TabPane>
+            <TabPane tab="忘记密码" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+          </Tabs>
         </div>
-      </Col>
-    </Row>
+      </div>
+    </div>
   );
 };
 
