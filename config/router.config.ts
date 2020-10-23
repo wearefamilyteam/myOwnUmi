39104e63bd1e25d9c574d97d23425e5f252a7387
MDZZ-LBJ <13041895753@163.com>
2020-10-24 02:03:51 +0800

    -

--- config/router.config.ts
+++ config/router.config.ts
@@ -40,7 +40,7 @@ export default [
             path: '/system/admin',
             routes: [
               {
-                path: '/system/admin/myTest2123ss44',
+                path: '/system/admin/myTest2123ss446',
                 component: '@/pages/admin/myTest2',
               },
             ],
